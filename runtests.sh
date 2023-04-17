8aee19ce75bb6f0ae03778f401192de7fa12f818
Mahesh Menon <menontmahesh@gmail.com>
2023-04-17 16:32:07 -0400

    redirects output to file but doesn't format

--- runtests.sh
+++ runtests.sh
@@ -2,13 +2,16 @@
 
 passes="passperms.txt"
 tests="testpaths.txt"
+results="results.txt"
 
 while read p; do
     while read t; do
         clang-14 -S -c -emit-llvm ${t} -o ${t}.bc
         opt-14 -enable-new-pm=0 -mem2reg ${p} ${t}.bc -S -o ${t}.ll
-        time clang-14 ${t}.ll
-        time ./a.out
+        echo ${p} >> "$results"
+        echo ${t} >> "$results"
+        (time clang-14 ${t}.ll)2>"$results"
+        (time ./a.out)2>"$results"
         du a.out
     done < "$tests"
 done < "$passes"
