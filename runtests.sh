5dfe34eee5eb400d0644f8a84b0d77ca94925b43
Mahesh Menon <menontmahesh@gmail.com>
2023-04-17 15:33:50 -0400

    kinda works i think

--- runtests.sh
+++ runtests.sh
@@ -5,10 +5,10 @@ tests="testpaths.txt"
 
 while read p; do
     while read t; do
-        clang -S -c -emit-llvm ${t} -o ${t}.bc
-        opt -enable-new-pm=0 -mem2reg ${p} ${t}.bc -S -o ${t}.ll
-        time clang ${t}.ll
-        time a.out
+        clang-14 -S -c -emit-llvm ${t} -o ${t}.bc
+        opt-14 -enable-new-pm=0 -mem2reg ${p} ${t}.bc -S -o ${t}.ll
+        time clang-14 ${t}.ll
+        time ./a.out
         du a.out
     done < "$tests"
 done < "$passes"
