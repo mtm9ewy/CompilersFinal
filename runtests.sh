bc29b71e36b126d40a193dc8b2f817b5f672e2fe
Mahesh Menon <menontmahesh@gmail.com>
2023-04-17 16:39:23 -0400

    fixed

--- runtests.sh
+++ runtests.sh
@@ -4,14 +4,16 @@ passes="passperms.txt"
 tests="testpaths.txt"
 results="results.txt"
 
+> "$results"
+
 while read p; do
     while read t; do
         clang-14 -S -c -emit-llvm ${t} -o ${t}.bc
         opt-14 -enable-new-pm=0 -mem2reg ${p} ${t}.bc -S -o ${t}.ll
         echo ${p} >> "$results"
         echo ${t} >> "$results"
-        (time clang-14 ${t}.ll)2>"$results"
-        (time ./a.out)2>"$results"
-        du a.out
+        (time clang-14 ${t}.ll)2>>"$results"
+        (time ./a.out)2>>"$results"
+        du a.out >> "$results"
     done < "$tests"
 done < "$passes"
