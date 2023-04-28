600f0ede01fd2224ef88715106564cd26b7e50b1
k-ivey <kivey1@gmail.com>
2023-04-28 14:04:39 -0400

    Update excluded nodes for SLURM

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -3,10 +3,17 @@
 start="$1"
 end="$2"
 
+# Settings for 16
 # Arbitrary choice, but it evenly divides total
-step=91
+# step=91
 # 16 * 15 * 14 * 13
-total=43680
+# total=43680
+
+# Settings for 12
+# 12 * 11 * 10 * 9 = 11880
+# 5 best optimizations from 16 case = 59400
+total=59400
+step=100
 
 end=$((end > total ? total : end))
 
@@ -14,7 +21,7 @@ while [[ $start -le $end ]]
 do
     last=$((start+step-1 < end ? start+step-1 : end))
     echo "Submitting a job for permutations ${start} through ${last}"
-    sbatch --exclude=adriatic01,adriatic02,adriatic03,adriatic04,adriatic05,adriatic06,affogato01,affogato02,affogato03,affogato04,affogato05,affogato11,affogato12,affogato13,affogato14,affogato15,ai01,ai02,ai03,ai04,ai05,ai06,ai07,ai08,ai09,ai10,cheetah01,cheetah02,cheetah03,doppio01,doppio02,doppio03,doppio04,doppio05,epona,heartpiece,hydro,jaguar01,jaguar02,jaguar03,jaguar04,lotus,lynx01,lynx02,lynx03,lynx04,lynx05,lynx06,lynx07,lynx08,lynx09,lynx10,lynx11,lynx12,optane01,panther01,pegasusboots,puma01,ristretto01,ristretto02,ristretto03,ristretto04,sds01,sds02,slurm1,slurm2,slurm3,slurm4,slurm5,titanx01,titanx02,titanx03,titanx04,titanx05,titanx06 runtests.sh "$start" "$last" 
+    sbatch --exclude=adriatic01,adriatic02,adriatic03,adriatic04,adriatic05,adriatic06,affogato01,affogato02,affogato03,affogato04,affogato05,affogato06,affogato07,affogato08,affogato09,affogato10,affogato11,affogato12,affogato13,affogato14,affogato15,ai01,ai02,ai03,ai04,ai05,ai06,ai07,ai08,ai09,ai10,cheetah01,cheetah02,cheetah03,doppio01,doppio02,doppio03,doppio04,doppio05,epona,heartpiece,hydro,jaguar01,jaguar02,jaguar03,jaguar04,lotus,lynx01,lynx02,lynx03,lynx04,lynx05,lynx06,lynx07,lynx08,lynx09,lynx10,lynx11,lynx12,optane01,panther01,pegasusboots,puma01,ristretto01,ristretto02,ristretto03,ristretto04,sds01,sds02,slurm1,slurm2,slurm3,slurm4,slurm5,titanx01,titanx02,titanx03,titanx04,titanx05,titanx06 runtests.sh "$start" "$last" 
     ((start+=step))
 done
 
